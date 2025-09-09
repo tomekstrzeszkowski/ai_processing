f50ee4255960b3453d6d08057e23e0b8dd568475
tstr <tomasz.strzeszkowski@gmail.com>
2025-09-09 19:22:47 +0200

    Add override template

--- rtsp/broadcaster/watcher/constants.go
+++ rtsp/broadcaster/watcher/constants.go
@@ -5,10 +5,10 @@ const SavePath = "./saved"
 // Persisting frames
 const ConvertFramesBeforeDays = 1
 
-const saveChunkSize = 5 * 1024 * 1024               //1024 * 1024 * 1024 // 1GB
-const convertedVideoSpace = 10 * 1024 * 1024 * 1024 //10 *1024 * 1024 * 1024 // 10GB
+const saveChunkSize = 1024 * 1024 * 1024            // 1GB
+const convertedVideoSpace = 10 * 1024 * 1024 * 1024 // 10GB
 
 const saveDirMaxSize = 100 * saveChunkSize
 
-const showWhatWasBefore = 90000 // 30 FPS * 60 seconds * 5 minutes = 90000 frames
-const showWhatWasAfter = 90000  // 30 FPS * 60 seconds * 5 minutes = 90000 frames
+const showWhatWasBefore = 100 // ~4 FPS * 60 seconds * 1 minutes = 240 frames
+const showWhatWasAfter = 100  // ~4 FPS * 60 seconds * 1 minutes = 240 frames
