18e8fc3e6648d5cc79627c48263ddc98824418d8
tstr <tomasz.strzeszkowski@gmail.com>
2025-11-10 19:10:33 +0100

    add setting icon

--- rtsp/viewer/app/(tabs)/_layout.tsx
+++ rtsp/viewer/app/(tabs)/_layout.tsx
@@ -8,7 +8,12 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs>
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: 'white',
+      tabBarStyle: {
+        backgroundColor: '#1a1a1a',
+      }
+    }}>
       <Tabs.Screen
         name="index"
         options={{
@@ -20,8 +25,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="videoList"
         options={{
-          title: 'video list',
+          title: 'Video List',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="settings.fill" color={color} />,
         }}
       />
     </Tabs>
